65f319068c551700de3d3f5d3b3d1617aaae0322
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-09-04 18:30:08 +0000

    sdfds

--- Easter.java
+++ Easter.java
@@ -3,5 +3,16 @@
 public class Easter{
     public static void main(String[] args) {
         //code goes here
+
+int y = 1986;
+int a = y % 19;
+int c = y % 100;
+int b = y/100;
+int d = b/4;
+int e = b%4;
+int f = (b+8)/25;
+int g = (b-f+1)/3;
+int h = (19 * a + b - d - g + 15)
     }
     }
+
